8aba6a8560ab5bf786e7259890b0d0d14e54ff69
Gustavo Cunha <gustavocunha@gmail.com>
2014-09-22 09:07:40 -0300

    Playing with basic operations

---
+++ OperationsPlayground.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var x = 10
+var y = 20
+
+var additionAnswer = x + y
+var multiplicationAnswer = x * y
+var subtractionAnswer = x - y
+var divisionAnswer = x / y
