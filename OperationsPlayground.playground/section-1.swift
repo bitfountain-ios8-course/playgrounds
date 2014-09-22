35121f46a5ed5dbe0f025a457b3a52b42ceb3a66
Gustavo Cunha <gustavocunha@gmail.com>
2014-09-22 09:15:25 -0300

    Playing with basic operations - Floats / Doubles

--- OperationsPlayground.playground/section-1.swift
+++ OperationsPlayground.playground/section-1.swift
@@ -9,3 +9,12 @@ var additionAnswer = x + y
 var multiplicationAnswer = x * y
 var subtractionAnswer = x - y
 var divisionAnswer = x / y
+
+var discountOnShoesAtStore = 0.3
+var priceOfShoes:Double = 33
+
+var priceAfterDiscount = priceOfShoes * (1 - discountOnShoesAtStore)
+
+var downPayment = 10.5
+
+priceAfterDiscount = priceAfterDiscount - downPayment
