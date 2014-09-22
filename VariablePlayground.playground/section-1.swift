7c2d2ff4d30ac0eee9510ba1b6d19b0a9b0b76ae
Gustavo Cunha <gustavocunha@gmail.com>
2014-09-22 08:29:11 -0300

    Starting to play with Playground for basic knowledge

---
+++ VariablePlayground.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var firstInteger = 0, secondInteger = 29
+
+var thirdInteger = -1000858
+
+var x = 5
+
+var discountAtDepartmentStore = 0.3
